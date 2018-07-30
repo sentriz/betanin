fa8b3c7978a1bb25e7663329b7b4a73249f5bcc5
sentriz <senan@senan.xyz>
2018-07-30 14:50:51 +0100

    a

--- app/client/vue_app/src/components/NavBar.vue
+++ app/client/vue_app/src/components/NavBar.vue
@@ -6,15 +6,41 @@
           src='../assets/logo.png',
           height='23'
         )
-    .navbar-menu
-      .navbar-start
+      a.navbar-burger(
+        role='button',
+        v-on:click='toggleShow()',
+        v-bind:class="{ 'is-active' : show }"
+      )
+        span
+        span
+        span
+    .navbar-menu(:class="{ 'is-active': show }")
+      .navbar-end
         router-link.navbar-item(to='/activity') Activity
         router-link.navbar-item(to='/history') History
         router-link.navbar-item(to='/settings') Settings
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggleShow () {
+      this.show = !this.show
+    }
+  }
+}
+</script>
+
 <style scoped>
  #logo {
     margin-right: 2.5rem;
-  }
+ }
+ .is-active {
+   font-weight: 600;
+ }
 </style>
