4ee9cf3a452f7e42501bbfba88d9a9166114eac4
sentriz <senan@senan.xyz>
2018-07-31 19:13:47 +0100

    a

--- app/client/vue_app/src/components/Downloads.vue
+++ app/client/vue_app/src/components/Downloads.vue
@@ -1,43 +1,51 @@
-<template>
-  <b-table :data="data" :columns="columns"></b-table>
+<template lang="pug">
+  b-table(
+    :data='downloads'
+    :loading='!downloadsDone'
+    :opened-detailed='openedDetails'
+    detailed
+    detail-key='id'
+    :has-detailed-visible='rowHasDetail'
+  )
+    template(slot-scope='props')
+      b-table-column(label='name') {{ props.row.name }}
+      b-table-column(label='progress')
+        progress.progress(
+          :value='props.row.percentDone'
+          max='100'
+        )
+      b-table-column(label='finished')
+        CheckMark(
+          v-if='props.row.isFinished === true'
+        )
+        XMark(
+          v-else
+        )
+    <!-- template(slot-scope='props', slot='detail') -->
+      <!-- p samsn -->
 </template>
 
 <script>
+import CheckMark from '@/components/icons/CheckMark.vue'
+import XMark from '@/components/icons/XMark.vue'
+import { mapGetters } from 'vuex'
 export default {
+  computed: mapGetters([
+    'downloads',
+    'downloadsDone'
+  ]),
+  components: {
+    CheckMark,
+    XMark
+  },
+  methods: {
+    rowHasDetail (row) {
+      return row.isFinished === true
+    }
+  },
   data () {
     return {
-      data: [
-        { 'id': 1, 'first_name': 'Jesse', 'last_name': 'Simmons', 'date': '2016-10-15 13:43:27', 'gender': 'Male' },
-        { 'id': 2, 'first_name': 'John', 'last_name': 'Jacobs', 'date': '2016-12-15 06:00:53', 'gender': 'Male' },
-        { 'id': 3, 'first_name': 'Tina', 'last_name': 'Gilbert', 'date': '2016-04-26 06:26:28', 'gender': 'Female' },
-        { 'id': 4, 'first_name': 'Clarence', 'last_name': 'Flores', 'date': '2016-04-10 10:28:46', 'gender': 'Male' },
-        { 'id': 5, 'first_name': 'Anne', 'last_name': 'Lee', 'date': '2016-12-06 14:38:38', 'gender': 'Female' }
-      ],
-      columns: [
-        {
-          field: 'id',
-          label: 'ID',
-          width: '40',
-          numeric: true
-        },
-        {
-          field: 'first_name',
-          label: 'First Name'
-        },
-        {
-          field: 'last_name',
-          label: 'Last Name'
-        },
-        {
-          field: 'date',
-          label: 'Date',
-          centered: true
-        },
-        {
-          field: 'gender',
-          label: 'Gender'
-        }
-      ]
+      openedDetails: []
     }
   }
 }
