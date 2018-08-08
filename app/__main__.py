43b8557827a6a5e35831b5a9cd4c3e7dba84a3d3
sentriz <senan@senan.xyz>
2018-08-08 21:06:43 +0100

    a

--- app/__main__.py
+++ app/__main__.py
@@ -5,38 +5,38 @@ from subprocess import Popen
 
 from .config import Config
 
+
 CLIENT_DIR = Config.CLIENT_DIR
 
 
 @click.group()
 def cli():
-    pass
+    sys.exit(1)
 
 
 def _bash(cmd, **kwargs):
-    click.echo('>>> {}'.format(cmd))
+    click.echo('>s>> {}'.format(cmd))
     return subprocess.call(cmd, env=os.environ, shell=True, **kwargs)
 
 
 @cli.command(help='server api dev server')
 def serve_api():
     '''serve api dev server'''
-    cmd = 'python run.py'
-    _bash(cmd)
+    _bash('python run.py')
 
 
 @cli.command(help='server client dev server')
 def serve_client():
     '''serve client dev server'''
-    cmd = 'npm run serve'
-    _bash(cmd, cwd=CLIENT_DIR)
+    _bash('npm run serve',
+          cwd=CLIENT_DIR)
 
 
 @cli.command(help='build client', name='build')
 def build():
     '''build client'''
-    cmd = 'npm run build'
-    _bash(cmd, cwd=CLIENT_DIR)
+    _bash('npm run build',
+          cwd=CLIENT_DIR)
 
 
 if __name__ == '__main__':
