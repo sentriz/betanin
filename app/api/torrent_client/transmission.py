af8eff414acc2920e2eafef03f70cc05bd2d0163
sentriz <senan@senan.xyz>
2018-08-08 16:28:29 +0100

    a

---
+++ app/api/torrent_client/transmission.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+from flask import request
+from flask_restplus import Api
+from transmission import Transmission
+
+from app.api.paths import config
+from app.api import torrent_client 
+
+
+_session = Transmission(
+    host=config.TRANSMISSION['host'],
+    port=config.TRANSMISSION['port'],
+    username=config.TRANSMISSION['username'],
+    password=config.TRANSMISSION['password'],
+    ssl=config.TRANSMISSION['ssl'],
+)
+
+
+def _torrent_is_done(torrent):
+    return torrent['leftUntilDone'] == 0
+
+
+def _torrent_is_music(torrent):
+    return torrent['downloadDir'] == config.DIRECTORY
+
+
+def _torrent_status_to_object(torrent):
+    if _torrent_is_done(torrent):
+        return torrent_client.Status.REMOTE_COMPLETED
+
+
+def _torrent_to_object(torrent):
+    return torrent_client.Torrent(
+        status=_torrent_status_to_object(torrent),
+        id=torrent['hashString'],
+        progress=torrent['percentDone'] * 100,
+        path=torrent['downloadDir'],
+    )
+
+
+def _should_process(torrent):
+    if not _torrent_is_music(torrent):
+        return False
+    # more
+    return True
+
+
+def get_torrents():
+    raw_torrents = _session('torrent-get',
+                            fields=[
+                                'name',
+                                'downloadDir', 
+                                'isFinished',
+                                'id',
+                                'hashString',
+                                'percentDone',
+                                'startDate',
+                                'leftUntilDone',
+                            ])
+    torrents = raw_torrents['torrents']
+    yield from map(_torrent_to_object, 
+                   filter(_should_process, torrents))
