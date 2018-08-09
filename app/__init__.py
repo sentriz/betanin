732f098c4b2da72f266cfab2abed63f9d3fe126c
sentriz <senan@senan.xyz>
2018-08-09 21:19:35 +0100

    a

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
+__import__('eventlet').monkey_patch()
+
 import os
-from flask import Flask
-from flask_socketio import SocketIO
 
 from app.api import api_rest
 from app.api import api_bp
 from app.client import client_bp
+
+from flask import Flask
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
 
 app.register_blueprint(api_bp)
 app.register_blueprint(client_bp)
 
+
 from . import config
 
-socketio = SocketIO(app)
+
+socketio = SocketIO(
+    app,
+    # engineio_logger=True,
+    # logger=True,
+    async_mode='eventlet',
+)
--- app/api/torrent_scheduler.py
+++ app/api/torrent_scheduler.py
@@ -1,5 +1,4 @@
 import time
-from threading import Thread
 
 from app import api
 from app.api import torrent_client
@@ -8,6 +7,7 @@ from app.api import beet_queue
 from app.api import events
 
 import requests
+import eventlet
 
 
 INTERVAL = 5
@@ -29,9 +29,8 @@ def _worker():
         for torrent in api.torrents:
             _process(torrent)
         events.torrents_grabbed()
-        time.sleep(INTERVAL)
+        eventlet.sleep(INTERVAL)
 
 
 def start_worker():
-    thread = Thread(target=_worker)
-    thread.start()
+    eventlet.spawn(_worker)
