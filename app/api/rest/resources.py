7a9eeeee89995ec3b6f257844b4ff034fbcb25f7
sentriz <senan@senan.xyz>
2018-08-09 19:55:12 +0100

    a

--- app/api/rest/resources.py
+++ app/api/rest/resources.py
@@ -1,12 +1,13 @@
+from app import api
 from app.api import api_rest
 from app.api import torrent_client
+from app.api.rest import response_models
 from app.api.rest.base import BaseResource
 from app.api.rest.namespaces import torrents_ns
-from app.api.rest import response_models
 
 
 @torrents_ns.route('/all')
 class TorrentsResource(BaseResource):
     @torrents_ns.marshal_list_with(response_models.torrent)
     def get(self):
-        return list(torrent_client.get_torrents())
+        return api.torrents
