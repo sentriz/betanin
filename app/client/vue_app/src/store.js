ecad0429addfec4085cd8618239bb92b395d04a8
sentriz <senan@senan.xyz>
2018-08-09 21:25:10 +0100

    a

--- app/client/vue_app/src/store.js
+++ app/client/vue_app/src/store.js
@@ -6,17 +6,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    downloads: []
+    downloads: [],
+    connected: false
   },
   getters: {
     downloads: state =>
       state.downloads,
     haveDownloads: state =>
-      state.downloads.length !== 0
+      state.downloads.length !== 0,
+    connected: state =>
+      state.connected
   },
   mutations: {
     setDownloads (state, downloads) {
       state.downloads = downloads
+    },
+    setConnected (state, connected) {
+      state.connected = connected
     }
   },
   actions: {
@@ -25,6 +31,16 @@ export default new Vuex.Store({
         .then(result => {
           commit('setDownloads', result)
         })
+    },
+    socket_grabbed: ({ commit, dispatch }) => {
+      dispatch('getDownloads')
+    },
+    socket_connect: ({ commit, dispatch }) => {
+      commit('setConnected', true)
+      dispatch('getDownloads')
+    },
+    socket_disconnect: ({ commit, dispatch }) => {
+      commit('setConnected', false)
     }
   }
 })
