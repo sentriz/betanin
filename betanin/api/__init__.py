a60503ee405c822f646b36efc11fb580fb120df2
sentriz <senan@senan.xyz>
2018-08-13 12:42:09 +0100

    a

--- betanin/api/__init__.py
+++ betanin/api/__init__.py
@@ -16,17 +16,6 @@ blueprint = Blueprint(
 )
 
 
-# TODO: use flask cors
-@blueprint.after_request
-def add_header(response):
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-    # required for webpack dev application to make requests to flask api
-    # from another host (localhost:8080)
-    if not current_app.config['PRODUCTION']:
-        response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
-
 from betanin.api.rest import namespaces
 from betanin.api.rest import resources
 
