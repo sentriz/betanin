191fb4d7f3476d56ae7f5415d5c2d4cc4ad292ab
sentriz <senan@senan.xyz>
2018-08-23 20:53:34 +0100

    finish modal with live preview

---
+++ betanin/client/vue_app/src/components/console/BaseConsole.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  pre
+    p(
+      v-for='line in lines(torrentID, lineLimit)'
+      :key='line.index'
+    ) {{ line.data }}
+</template>
+
+<script>
+// imports
+import { mapGetters } from 'vuex'
+// export
+export default {
+  props: [
+    'torrentID',
+    'lineLimit'
+  ],
+  computed: mapGetters([
+    'lines'
+  ])
+}
+</script>
