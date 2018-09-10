0509e95461f1a3088fd135d3bdcc75be632b4065
sentriz <senan@senan.xyz>
2018-09-10 20:36:28 +0100

    add simple remote list

--- betanin/client/src/views/Settings.vue
+++ betanin/client/src/views/Settings.vue
@@ -1,9 +1,27 @@
+      <!-- b-icon(icon='menu-down') -->
 <template lang="pug">
-  p settings here
+  b-dropdown(hoverable)
+    button.button.is-info(slot='trigger')
+      span {{ remote }}
+      span &nbsp;v
+    b-dropdown-item(
+      v-for='remote in remotes'
+      :key='remote'
+    ) {{ remote }}
 </template>
 
 <script>
+const defaultRemote = 'transmission'
 export default {
-  name: 'settings'
+  data () {
+    return {
+      remote: defaultRemote,
+      remotes: [
+        defaultRemote,
+        'deluge',
+        'rtorrent'
+      ]
+    }
+  }
 }
 </script>
