575ebf4584b765cb2061357ce6cd99635d1f893e
sentriz <senan@senan.xyz>
2018-10-05 05:38:16 +0100

    use vuex for remote config

--- betanin/client/src/store/state.js
+++ betanin/client/src/store/state.js
@@ -1,5 +1,14 @@
 export default {
   downloads: [],
   lines: {},
+  remotes: [
+    {
+      id: 3,
+      type: 'transmission',
+      config: {
+        hostname: 'damn daniel'
+      }
+    }
+  ],
   connected: false
 }
