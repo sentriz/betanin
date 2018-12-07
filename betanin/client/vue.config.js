76878f9235a4e05d0ec6f5e5f6acf51635fb12d8
sentriz <senan@senan.xyz>
2018-12-07 22:30:04 +0000

    add dev logo

--- betanin/client/vue.config.js
+++ betanin/client/vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        '__SOURCE_COMMIT__': JSON.stringify(process.env.SOURCE_COMMIT || '')
+        '__SOURCE_COMMIT__': JSON.stringify(process.env.SOURCE_COMMIT || ''),
+        '__NODE_ENV__': JSON.stringify(process.env.NODE_ENV || '')
       })
     ]
   }
