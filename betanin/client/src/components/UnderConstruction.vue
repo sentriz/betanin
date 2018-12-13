acb7bc91d9b0daf1fa951e762e244fc11c1952f6
Zak Freeman <22828384+izaakf@users.noreply.github.com>
2018-12-13 16:18:28 +0000

    Create UnderConstruction.vue
    Very useful to show users that a page is under construction. just place it on a page and you are good to go!!!
    
    demo: http://jsfiddle.net/8re7jf56/

---
+++ betanin/client/src/components/UnderConstruction.vue
@@ -0,0 +1,20 @@
+<template lang='pug'>
+  div#construction
+    img.image(src='https://media1.tenor.com/images/eb1b27863813653543914d222ceb9cd0/tenor.gif?itemid=5548554')#content
+    p !!!this site is under construction!!!#content
+    img.image(src='https://media1.tenor.com/images/eb1b27863813653543914d222ceb9cd0/tenor.gif?itemid=5548554')#content
+</template>
+
+<style lange='scss' scoped>
+  #construction {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  #content {
+    z-index: 10001;
+    text-align: center;
+    width: 30%;
+    display: inline-block;
+  }
+</stlye>
