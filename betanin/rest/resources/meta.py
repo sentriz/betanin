bb9920d16ff3a5ec8aa7163383d968387086e527
sentriz <senan@senan.xyz>
2019-03-13 12:49:39 +0000

    secure meta info endpoint

--- betanin/rest/resources/meta.py
+++ betanin/rest/resources/meta.py
@@ -1,12 +1,12 @@
 # betanin
 from betanin import system_info
-from betanin.rest.base import BaseResource
+from betanin.rest.base import SecureResource
 from betanin.rest.models import response as resp_models
 from betanin.rest.namespaces import meta_ns
 
 
 @meta_ns.route('/system_info')
-class metaResource(BaseResource):
+class metaResource(SecureResource):
     @staticmethod
     @meta_ns.marshal_with(resp_models.system_info)
     def get():
