fc5c3b0f95d3dd901a9ca6b0884c7e2c39e558a5
sentriz <senan@senan.xyz>
2019-04-08 18:11:08 +0100

    read psy tty with gevent

--- betanin/jobs/serve_web.py
+++ betanin/jobs/serve_web.py
@@ -1,12 +1,10 @@
 # 3rd party
+import gevent
 from loguru import logger
 from gevent.pywsgi import WSGIServer
-
-# betanin
-from betanin.extensions import socketio
 
 
 def start(flask_app, port):
     http_server = WSGIServer(('', port), log=None, application=flask_app)
     logger.info(f'starting wsgi server on port {port}')
-    return socketio.start_background_task(http_server.start)
+    return gevent.spawn(http_server.start)
