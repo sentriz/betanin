3df13149050ea5067fc2dbf5611edea8ff05ffc7
sentriz <senan@senan.xyz>
2019-04-25 17:03:57 +0100

    show live icon when scrolled

--- betanin_client/src/components/console/BaseConsole.vue
+++ betanin_client/src/components/console/BaseConsole.vue
@@ -1,8 +1,5 @@
 <template lang="pug">
   pre(v-chat-scroll)
-    #live-box(v-show='isLive')
-      span#live-fade &#x25A0
-      span#live-text live
     p(
       v-for='line in getByID[torrentID]'
       :key='line.index'
@@ -46,34 +43,10 @@ export default {
     height: 50vh;
     position: relative;
     overflow-x: hidden;
+    overflow-y: scroll;
   }
   p {
     font-size: 11px;
-    color: white;
-  }
-  @keyframes fadeinout {
-    0%, 60%, 100% {opacity: 1;}
-    80%           {opacity: 0;}
-  }
-  #live-box {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-    margin: 0.75rem;
-    font-size: 14px;
-    word-spacing:  3px;
-    padding: 0 0.5rem;
-    border-radius: 2px;
-    background-color: rgba(255, 255, 255, 0.1);
-  }
-  #live-fade {
-    color: red;
-    animation: fadeinout 2s;
-    animation-iteration-count: infinite;
-    margin-right: 4px;
-    font-size: 18px;
-  }
-  #live-text {
     color: white;
   }
 </style>
